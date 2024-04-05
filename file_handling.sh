c43dd4a40163861d1b9e37f9383279f058bce1ae
Santosh Adhikari <86250573+santos-adh114@users.noreply.github.com>
2024-04-05 11:55:58 -0500

    Update file_handling.sh

--- file_handling.sh
+++ file_handling.sh
@@ -6,8 +6,10 @@ rm -r -f <directory_path> (order of -r -f does not matter. We can use rm -f -r <
 ## Following command looks for file named "file_name" and deletes if from all directories and sub-directories
 find . -name "file_name" -delete
 
-# following command finds all the files inside the given directory and its subdirectories for file with extension .sh and stores
-# the list in a data file list.txt
+FINDING A FILE
+
+# following command finds all the files inside the given directory and its subdirectories for file 
+#with extension .sh and stores the list in a data file list.txt
 
    find . -type f -name '*.sh'>list.txt  # use >> for append mode. Especially useful during looping 
 
@@ -21,6 +23,11 @@ find . -name "file_name" -delete
       echo
     done
 
+In the repo there is a file called "find_replace.sh". This code searches for a file of given name 
+in the given directory and writes to a file for complete path of occurance of each file. Once
+the file is written, it then reads each line of the file and searches for an pharase to find and 
+replace. Finally, it deletes the file where the path were written 
+(uncommenting this line would keep the file)
 
 # Following script looks for a particular file called 'lobsterout' in the folder. If it doesn't see that file then the
 # code executes certain functions. This can be helpful in future experience.
